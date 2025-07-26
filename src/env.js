f4eccb450dcec5c95c8318922c1bb2622c863f4e
okafrancois <itoutouberny@gmail.com>
2025-07-26 23:28:36 +0200

    Add DIRECT_URL environment variable to schema and env configuration
    - Introduced DIRECT_URL in the Prisma schema for database connection.
    - Updated env.js to include DIRECT_URL as a required environment variable for better configuration management.

--- src/env.js
+++ src/env.js
@@ -12,6 +12,7 @@ export const env = createEnv({
         ? z.string()
         : z.string().optional(),
     DATABASE_URL: z.string().url(),
+    DIRECT_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -33,6 +34,7 @@ export const env = createEnv({
   runtimeEnv: {
     AUTH_SECRET: process.env.AUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
   /**
